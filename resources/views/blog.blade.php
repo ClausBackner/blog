cf032766302c67c90bc19a8693173275d6ab90da
ClausBackner <l-ovo12@hotmail.com>
2024-09-25 22:28:57 -0500

    actualizacion 3
    se quiere agregar una mejor interfaz con una mejor estructura pero se presenta fallos

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,11 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link rel="stylesheet" href="{{asset('css/stilomagen.css')}}">
     <title>Blog</title>
 </head>
 <body>
 @include('partials.navegation')
-<h1>Blog</h1>
+<h2 class="titulo">Slider automatico</h2>
+<div class="slider-box">
+    <ul>
+        <li>
+            <img src="https://www.colgate.com/content/dam/cp-sites/oral-care/oral-care-center/global/article/gscp/latam/cheerful-man-dentist-talking.jpg" alt="">
+            <div class="texto">
+                <h2>imagen 1</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium blanditiis consectetur, dolorem dolores ea eaque
+                    exercitationem id illum laborum magnam, natus nisi nulla perspiciatis
+                    soluta, totam velit vero voluptas voluptate.
+                </p>
+            </div>
+        </li>
+        <li>
+            <img src="https://www.clinicasonrisasegura.pe/wp-content/uploads/2023/08/Extraccion-de-dientes.webp" alt="">
+            <div class="texto">
+                <h2>imagen 2</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium blanditiis consectetur, dolorem dolores ea eaque
+                    exercitationem id illum laborum magnam, natus nisi nulla perspiciatis
+                    soluta, totam velit vero voluptas voluptate.
+                </p>
+            </div>
+        </li>
+        <li>
+            <img src="https://soluciondental.pe/wp-content/uploads/2019/01/profilaxis-dental-profunda.jpg" alt="">
+            <div class="texto">
+                <h2>imagen 3</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium blanditiis consectetur, dolorem dolores ea eaque
+                    exercitationem id illum laborum magnam, natus nisi nulla perspiciatis
+                    soluta, totam velit vero voluptas voluptate.
+                </p>
+            </div>
+        </li>
+        <li>
+            <img src="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_590,h_350/https://www.alfadent.com.pe/wp-content/uploads/2020/04/590x350-restauraciones-2.jpg" alt="">
+            <div class="texto">
+                <h2>imagen 4</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium blanditiis consectetur, dolorem dolores ea eaque
+                    exercitationem id illum laborum magnam, natus nisi nulla perspiciatis
+                    soluta, totam velit vero voluptas voluptate.
+                </p>
+            </div>
+        </li>
+    </ul>
+</div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="{{asset('css/styleimag.css')}}">
     <title>Inicio</title>
 </head>
 <body>
 @include('partials.navegation')
 
-<h1>INICIO</h1>
+<div id="slider">
+    <img src="https://www.colgate.com/content/dam/cp-sites/oral-care/oral-care-center/global/article/gscp/latam/cheerful-man-dentist-talking.jpg" alt="">
+    <img src="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_590,h_350/https://www.alfadent.com.pe/wp-content/uploads/2020/04/590x350-restauraciones-2.jpg" alt="">
+    <img src="https://www.postgradounab.cl/wp-content/uploads/2022/10/400x243-Conoce-los-5-tipos-de-cirugia-dental-mas-requeridas.jpg" alt="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF90aE7_aKNuBnCZNS2HR1PF9xb7A-sRLBMg&s" alt="">
+    <img src="https://cdn.prod.website-files.com/66183cdcc69331bd58af1786/66646552144fb707a8d2b6a0_22-1024x731.jpeg" alt="">
+    <img src="https://www.clinicasonrisasegura.pe/wp-content/uploads/2023/08/Extraccion-de-dientes.webp" alt="">
+    <img src="https://soluciondental.pe/wp-content/uploads/2019/01/profilaxis-dental-profunda.jpg" alt="">
+</div>
 
+<script  src="{{asset('js/jslider.js')}}"></script>
 </body>
 </html>
